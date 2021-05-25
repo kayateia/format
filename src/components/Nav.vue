5c01ee16ebb4544692fd585899ee23288703a62d
Kayateia <github@kayateia.net>
2021-05-25 00:38:49 -0400

    wip: factor out nav bar

---
+++ src/components/Nav.vue
@@ -0,0 +1,51 @@
+<!--
+//
+// Copyright (c) 2021 Kayateia
+// Please see README.md in the root of this project for licence info.
+//
+-->
+
+<template>
+  <div id="nav">
+    <b-navbar id="navbar-bar" toggleable="lg" type="dark">
+      <b-navbar-brand href="#">[ f o r m a t ]</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+              <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+          </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+#navbar-bar {
+  background-color: #162e47;
+}
+
+</style>
